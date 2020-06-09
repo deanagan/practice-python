47e0197bbf40829c834901613f6f3ac3ee5a4ab1
Dean Agan <agandfr@gmail.com>
2020-06-09 17:26:43 +1000

    Add better solution than mine.

--- nearestvalue/nearestvalue.py
+++ nearestvalue/nearestvalue.py
@@ -2,6 +2,8 @@ from functools import cmp_to_key
 
 def nearest_value(values: set, one: int) -> int:
     return sorted(values, key = cmp_to_key(lambda a,b: abs(a - one) - abs(b - one) or (a-b) ))[0]
+    # Better
+    # return min(values, key=lambda n: (abs(one - n), n))
 
 
 if __name__ == '__main__':
