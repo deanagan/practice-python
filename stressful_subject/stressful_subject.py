09e191dd31e31793cc2303195a989457d5bbb23a
Dean Agan <agandfr@gmail.com>
2020-10-09 17:39:38 +1100

    Use endswith to be more verbose. A slice works fine but I think endswith is clearer

--- stressful_subject/stressful_subject.py
+++ stressful_subject/stressful_subject.py
@@ -1,7 +1,7 @@
 from itertools import groupby
 
 def is_stressful(subj):
-    if subj.isupper() or subj[-3:] == "!!!":
+    if subj.isupper() or subj.endswith("!!!"):
         return True
     filtered_subj = ''.join([i.lower() for i,_ in groupby(subj) if i.isalpha()])
     return any(red_word in filtered_subj for red_word in ("help", "asap", "urgent"))
