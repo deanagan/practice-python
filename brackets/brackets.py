cd955ea12aecf50b690230a22b9dfe4671458f57
Dean Agan <agandfr@gmail.com>
2020-10-11 15:18:04 +1100

    Cleanup brackets solution

--- brackets/brackets.py
+++ brackets/brackets.py
@@ -1,15 +1,12 @@
 
 def are_brackets_valid(expression) -> bool:
-    bracket_open = ('{', '[', '(')
-    bracket_close = ('}', ']', ')')
-    stack = []
-    for ch in expression:
-        if ch in bracket_open:
-            stack.append(ch)
-        elif ch in bracket_close:
-            if len(stack) > 0 and stack[-1] == bracket_open[bracket_close.index(ch)]:
-                stack.pop()
-            else:
-                return False
 
-    return len(stack) == 0
+    stack = ['']
+    brackets = { '{': '}', '[': ']', '(': ')'}
+    for ch in expression:
+        if ch in brackets:
+            stack.append(brackets[ch])
+        elif ch in brackets.values() and ch != stack.pop():
+            return False
+
+    return stack == ['']
