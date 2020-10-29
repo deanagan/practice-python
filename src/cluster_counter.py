9f6a80280e1214b1c0b1603e36f239fe22053381
Dean Agan <agandfr@gmail.com>
2020-10-29 19:58:07 +1100

    Slight refactor to solution

--- src/cluster_counter.py
+++ src/cluster_counter.py
@@ -13,19 +13,18 @@ class Node:
         col_dist = abs(self._y - y)
         return row_dist < 2 and col_dist < 2
 
-def get_next_node(clusters: ClusterMap) -> Tuple[int, int, int]:
-    for row, row_elements in enumerate(clusters):
-        for col, n in enumerate(row_elements):
-            yield (row, col, n)
+def get_next_valid_node(clusters: ClusterMap) -> Tuple[int, int, int]:
+    yield from ((row, col, n) for row, row_elements in enumerate(clusters)
+                    for col, n in enumerate(row_elements) if n == 1)
+
 
 def count_cluster(clusters: ClusterMap) -> int:
     node_id = 0
     nodes = {}
-    for row, col, n in get_next_node(clusters):
-        if n == 1:
-            neighbor = next((v for n in nodes.values() for v in n if v.is_neighbor(row,col)), None)
-            nodes.setdefault(cluster_id := neighbor.id if neighbor else node_id, []).append(Node(row, col, cluster_id))
-            if not neighbor:
-                node_id += 1
+    for row, col, n in get_next_valid_node(clusters):
+        neighbor = next((v for n in nodes.values() for v in n if v.is_neighbor(row,col)), None)
+        nodes.setdefault(cluster_id := neighbor.id if neighbor else node_id, []).append(Node(row, col, cluster_id))
+        if not neighbor:
+            node_id += 1
 
     return len(nodes.keys())
