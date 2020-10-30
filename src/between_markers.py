360f1a2f310f2befb5371495229a38c2869e6a73
Dean Agan <agandfr@gmail.com>
2020-10-30 20:51:38 +1100

    Add passing solution

--- src/between_markers.py
+++ src/between_markers.py
@@ -1,6 +1,9 @@
-
+from itertools import dropwhile, takewhile
 
 
 def between_markers(text: str, begin: str, end: str) -> str:
+    return text[text.index(begin) + 1: text.index(end)]
 
-    return ''
+
+if __name__ == '__main__':
+    between_markers('What is >apple<', '>', '<')# == "apple", "One sym"
