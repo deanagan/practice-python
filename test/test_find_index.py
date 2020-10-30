54eb5abb54d90539e2c067049e58636cd0638007
Dean Agan <agandfr@gmail.com>
2020-10-31 09:06:08 +1100

    Add test

---
+++ test/test_find_index.py
@@ -0,0 +1,10 @@
+import pytest
+
+from src.find_second_index import second_index
+
+def test_find_second_index():
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
