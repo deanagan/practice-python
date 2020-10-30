c2b821302b1cc4c6284ec82d392e31ba2d3311da
Dean Agan <agandfr@gmail.com>
2020-10-30 20:52:43 +1100

    Add another test. Failed.

--- test/test_between_markers.py
+++ test/test_between_markers.py
@@ -3,3 +3,5 @@ from src.between_markers import between_markers
 
 def test_between_markers():
     assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers("<head><title>My new site</title></head>",
+                           "<title>", "</title>") == "My new site", "HTML"
