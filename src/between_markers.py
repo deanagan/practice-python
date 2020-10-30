4060334317b406b9580c009f119d9ab4ffed6489
Dean Agan <agandfr@gmail.com>
2020-10-30 20:55:41 +1100

    Modified to pass

--- src/between_markers.py
+++ src/between_markers.py
@@ -2,8 +2,8 @@ from itertools import dropwhile, takewhile
 
 
 def between_markers(text: str, begin: str, end: str) -> str:
-    return text[text.index(begin) + len(begin): text.index(end)]
+    return text[text.index(begin) + len(begin) if begin in text else 0: text.index(end)]
 
 
 if __name__ == '__main__':
-    between_markers('What is >apple<', '>', '<')# == "apple", "One sym"
+    between_markers('No[/b] hi', '[b]', '[/b]')
