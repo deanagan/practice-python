db565dc71fc6d308eb39eebc06c656c93add65b3
Dean Agan <agandfr@gmail.com>
2020-10-30 15:57:20 +1100

    Add another test. Still passed.

--- test/test_backward_string_by_word.py
+++ test/test_backward_string_by_word.py
@@ -6,3 +6,4 @@ def test_backward_string_by_word():
     assert backward_string_by_word('') == ''
     assert backward_string_by_word('world') == 'dlrow'
     assert backward_string_by_word('hello world') == 'olleh dlrow'
+    assert backward_string_by_word('hello   world') == 'olleh   dlrow'
