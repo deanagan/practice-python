a92a4d0cce4f7939b1bfe973c89cb50640388161
Dean Agan <agandfr@gmail.com>
2020-10-30 16:02:03 +1100

    Refactor

--- src/backward_string_by_word.py
+++ src/backward_string_by_word.py
@@ -1,4 +1,4 @@
 from itertools import groupby
 
 def backward_string_by_word(text: str) -> str:
-    return ''.join(''.join(v)[::-1] for _,v in groupby(text, lambda w: w.isspace()))
+    return ' '.join([word[::-1] for word in text.split(' ')])
