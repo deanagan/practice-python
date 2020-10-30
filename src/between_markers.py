a519d1db78e683cb2ee0aa75cc402ac76f7a5642
Dean Agan <agandfr@gmail.com>
2020-10-30 20:57:51 +1100

    Add solution to no test. Now passing.

--- src/between_markers.py
+++ src/between_markers.py
@@ -2,7 +2,8 @@ from itertools import dropwhile, takewhile
 
 
 def between_markers(text: str, begin: str, end: str) -> str:
-    return text[text.index(begin) + len(begin) if begin in text else 0: text.index(end)]
+    return text[text.index(begin) + len(begin) if begin in text else 0:
+                text.index(end) if end in text else None]
 
 
 if __name__ == '__main__':
