e7b41ccb1462111a27cc58f789b0f0a3e3bae607
Dean Agan <agandfr@gmail.com>
2020-10-31 18:19:21 +1100

    Removed flooring

--- src/sun_angle.py
+++ src/sun_angle.py
@@ -1,8 +1,8 @@
 from datetime import datetime as dt
-from math import floor
 
-def sun_angle(time: str) -> [int, str]:
+
+def sun_angle(time: str) -> [float, str]:
     ref_time = dt.strptime('06:00', '%H:%M')
     fmt_time = dt.strptime(time, '%H:%M')
-    angle = floor((fmt_time - ref_time).seconds // 60 * 0.25)
+    angle = (fmt_time - ref_time).seconds / 60 * 0.25
     return angle if 0 <= angle <= 180 else "I don't see the sun!"
