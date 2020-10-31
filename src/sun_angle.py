26559ed78b72ec68f6dbdf9673ec4c34dbebe9d9
Dean Agan <agandfr@gmail.com>
2020-10-31 18:12:04 +1100

    Add solution to pass

--- src/sun_angle.py
+++ src/sun_angle.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+from math import floor
 
 def sun_angle(time: str) -> [int, str]:
-    return time
+    return floor(((dt.strptime(time, '%H:%M') - dt.strptime('06:00', '%H:%M')).seconds // 60) * 0.25)
