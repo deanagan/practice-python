8c382d1080172971c2949cc69652bc9b2b624555
Dean Agan <agandfr@gmail.com>
2020-10-31 18:17:43 +1100

    Pass out of angle

--- src/sun_angle.py
+++ src/sun_angle.py
@@ -2,4 +2,7 @@ from datetime import datetime as dt
 from math import floor
 
 def sun_angle(time: str) -> [int, str]:
-    return floor(((dt.strptime(time, '%H:%M') - dt.strptime('06:00', '%H:%M')).seconds // 60) * 0.25)
+    ref_time = dt.strptime('06:00', '%H:%M')
+    fmt_time = dt.strptime(time, '%H:%M')
+    angle = floor((fmt_time - ref_time).seconds // 60 * 0.25)
+    return angle if 0 <= angle <= 180 else "I don't see the sun!"
