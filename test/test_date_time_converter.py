adb4d5c12975ff14fc19044b4a7d5e0dbc6d6a21
Dean Agan <agandfr@gmail.com>
2020-11-02 18:22:08 +1100

    Add another test

--- test/test_date_time_converter.py
+++ test/test_date_time_converter.py
@@ -3,6 +3,7 @@ from src.date_time_converter import date_time
 
 @pytest.mark.parametrize("input, expected, note",
     [("01.01.2000 00:00", "1 January 2000 year 0 hours 0 minutes", "Somebody was born") ,
-     ("09.05.1945 06:30", "9 May 1945 year 6 hours 30 minutes", "Victory")])
+     ("09.05.1945 06:30", "9 May 1945 year 6 hours 30 minutes", "Victory"),
+     ("20.11.1990 03:55", "20 November 1990 year 3 hours 55 minutes", "Somebody was born")])
 def test_date_time_converter(input, expected, note):
     assert date_time(input) == expected, note
