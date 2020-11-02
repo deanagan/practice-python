b737a30de32e9f2a49eccd0bee1893dd65aea484
Dean Agan <agandfr@gmail.com>
2020-11-02 18:30:31 +1100

    Add plurality solution

--- src/date_time_converter.py
+++ src/date_time_converter.py
@@ -3,4 +3,6 @@ from datetime import datetime as dt
 
 def date_time(time: str) -> str:
     dtc = dt.strptime(time, "%d.%m.%Y %H:%M")
-    return f"{dtc.day} {dtc.strftime('%B')} {dtc.year} year {dtc.hour} hours {dtc.minute} minutes"
+    hour = 'hour' if dtc.hour == 1 else 'hours'
+    minute = 'minute' if dtc.minute == 1 else 'minutes'
+    return f"{dtc.day} {dtc.strftime('%B')} {dtc.year} year {dtc.hour} {hour} {dtc.minute} {minute}"
