1158883ae73b94103a5241996ea1ab88f980117e
Dean Agan <agandfr@gmail.com>
2020-11-05 15:57:52 +1100

    Add test

---
+++ test/test_is_all_upper.py
@@ -0,0 +1,10 @@
+import pytest
+
+from src.is_all_upper import is_all_upper
+
+
+@pytest.mark.parametrize('input,expected', [
+    ('ALL UPPER', True)
+])
+def test_is_all_upper(input, expected):
+    assert input == expected
