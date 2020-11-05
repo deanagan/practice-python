3052fefcf13e38d008e84bafa5c75849b6f9ffce
Dean Agan <agandfr@gmail.com>
2020-11-05 15:59:22 +1100

    Fixed test

--- test/test_is_all_upper.py
+++ test/test_is_all_upper.py
@@ -4,7 +4,7 @@ from src.is_all_upper import is_all_upper
 
 
 @pytest.mark.parametrize('input,expected', [
-    ('ALL UPPER', True)
+    ('ALL UPPER', True),
 ])
 def test_is_all_upper(input, expected):
-    assert input == expected
+    assert is_all_upper(input) == expected
