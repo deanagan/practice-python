1378e7d38ae5667d36f0901ccbd9e718263b4616
Dean Agan <agandfr@gmail.com>
2020-11-05 16:38:42 +1100

    Refactoring

--- src/is_all_upper.py
+++ src/is_all_upper.py
@@ -2,4 +2,5 @@
 
 
 def is_all_upper(text: str) -> bool:
-    return True
+    #return bool(filtered := [ch for ch in text if ch.isalpha()]) and all(ch.isupper() for ch in filtered)
+    return False if text.lower() == text else text == text.upper()
