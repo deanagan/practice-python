68741b1c2229af4e3d31bb20070056029c75da75
Dean Agan <agandfr@gmail.com>
2020-11-07 17:40:57 +1100

    Add split string test

---
+++ test/test_split_string.py
@@ -0,0 +1,15 @@
+import pytest
+from typing import Iterable
+ResultType = Iterable[str]
+
+from src.split_string import split_pairs
+
+@pytest.mark.parametrize('input, expected', [
+    ('abcd', ['ab', 'cd']),
+    ('abc', ['ab', 'c_']),
+    ('abcdf', ['ab', 'cd', 'f_']),
+    ('a', ['a_']),
+    ('', []),
+])
+def test_split_string_by_pairs(input: str, expected: ResultType):
+    assert split_pairs(input) == expected
