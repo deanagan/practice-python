2cde8bfea62983643c8153c7d79e0b1b32379fc9
Dean Agan <agandfr@gmail.com>
2020-11-09 17:49:01 +1100

    Add initial solution

--- src/is_all_upper.py
+++ src/is_all_upper.py
@@ -4,3 +4,6 @@
 def is_all_upper(text: str) -> bool:
     #return bool(filtered := [ch for ch in text if ch.isalpha()]) and all(ch.isupper() for ch in filtered)
     return False if text.lower() == text else text == text.upper()
+
+def is_all_upper_or_empty(text: str) -> bool:
+    return not text or is_all_upper(text)
