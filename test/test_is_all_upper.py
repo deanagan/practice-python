6f085f8c733e4fddd4b43afebe4549e73f923e4b
Dean Agan <agandfr@gmail.com>
2020-11-09 17:48:51 +1100

    Create test for all or empty

--- test/test_is_all_upper.py
+++ test/test_is_all_upper.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.is_all_upper import is_all_upper
+from src.is_all_upper import is_all_upper, is_all_upper_or_empty
 
 
 @pytest.mark.parametrize('input,expected', [
@@ -13,3 +13,13 @@ from src.is_all_upper import is_all_upper
 ])
 def test_is_all_upper(input, expected):
     assert expected == is_all_upper(input)
+
+
+@pytest.mark.parametrize('input,expected', [
+    ('ALL UPPER', True),
+    ('all lower', False),
+    ('mixed UPPER and lower', False),
+    ('', True),
+])
+def test_is_all_upper_or_empty(input, expected):
+    assert expected == is_all_upper_or_empty(input)
