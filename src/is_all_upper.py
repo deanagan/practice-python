57a64a3ed4def84f43d7a37f1598c99ad7bcba46
Dean Agan <agandfr@gmail.com>
2020-11-09 17:58:24 +1100

    Change solution to accomodate digits with no upper case characters

--- src/is_all_upper.py
+++ src/is_all_upper.py
@@ -2,8 +2,7 @@
 
 
 def is_all_upper(text: str) -> bool:
-    #return bool(filtered := [ch for ch in text if ch.isalpha()]) and all(ch.isupper() for ch in filtered)
     return False if text.lower() == text else text == text.upper()
 
 def is_all_upper_or_empty(text: str) -> bool:
-    return not text.strip() or is_all_upper(text)
+    return all(fch.isupper() for fch in filter(lambda ch: ch.isalpha(), text))
