9b7408eda2eaabfa54e2eb9f17a32eb821b7beba
Dean Agan <agandfr@gmail.com>
2020-11-09 18:13:05 +1100

    Add implementation with no numpy

--- src/replace_first.py
+++ src/replace_first.py
@@ -1,5 +1,10 @@
 import numpy as np
 from typing import Iterable
+from collections import deque
 
 def replace_first_np(items: list) -> Iterable:
     return list(i for i in np.roll(items, -1))
+
+
+def replace_first(items: list) -> Iterable:
+    return items[1:] + items[0:1]
