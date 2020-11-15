621eb2871f89f64352071fc225583a61af64aba1
Dean Agan <agandfr@gmail.com>
2020-11-15 16:20:12 +1100

    Pass tests

--- src/longest_increasing_path.py
+++ src/longest_increasing_path.py
@@ -1,6 +1,43 @@
-
+from itertools import product, takewhile
 from typing import List, Tuple
+from collections import deque
+
+
 Matrix = List[List[int]]
+class MatrixRunner:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.n_rows = len(matrix)
+        self.n_cols = len(matrix[0])
+
+    def neighbors(self, location, rows, cols):
+        current_row, current_col = location
+        yield from [(current_row + r, current_col + c) for r,c in [(0,1), (1,0), (0,-1), (-1,0)]
+                    if 0 <= (current_row + r) < rows and
+                       0 <= (current_col + c) < cols]
+
+    def longest_path(self):
+        cache, visited, lengths = {}, {}, []
+
+        for row, col in product(range(self.n_rows), range(self.n_cols)):
+            lengths.append(self.depth_first_search((row, col), cache, visited))
+
+        return max(lengths)
+
+    def depth_first_search(self, location, cache, visited):
+        visited[location] = True
+        location_value = self.matrix[location[0]][location[1]]
+
+        for n in self.neighbors(location, self.n_rows, self.n_cols):
+            n_value = self.matrix[n[0]][n[1]]
+            if location_value < n_value and n_value not in visited:
+                return 1 + self.depth_first_search(n, cache, visited)
+
+        return 1
+
+
 
 def longest_increasing_path(matrix: Matrix) -> int:
-    return 4
+
+    mr = MatrixRunner(matrix)
+    return mr.longest_path()
--- test/test_longest_increasing_path.py
+++ test/test_longest_increasing_path.py
@@ -4,7 +4,7 @@ from src.longest_increasing_path import longest_increasing_path
 
 @pytest.mark.parametrize(('input, expected'), [
     ([[9,9,4], [6,6,8], [2,1,1]], 4),
-   # ([[3,4,5], [3,2,6], [2,2,1]], 4),
+    ([[3,4,5], [3,2,6], [2,2,1]], 4),
 ])
 def test_long_non_repeat(input, expected):
     assert longest_increasing_path(input) == expected
