fa843b43c5aa143b6a644fa283de84fd0a13370b
Dean Agan <agandfr@gmail.com>
2020-11-15 21:27:17 +1100

    Add failing test

--- test/test_longest_increasing_path.py
+++ test/test_longest_increasing_path.py
@@ -6,6 +6,7 @@ from src.longest_increasing_path import longest_increasing_path
     ([[9,9,4], [6,6,8], [2,1,1]], 4),
     ([[3,4,5], [3,2,6], [2,2,1]], 4),
     ([], 0),
+    ([[7,7,5],[2,4,6],[8,2,0]], 4),
 ])
 def test_long_non_repeat(input, expected):
     assert longest_increasing_path(input) == expected
