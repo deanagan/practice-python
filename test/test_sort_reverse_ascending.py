6cb37250aa5d12e55548fb21dc60d8d3411837d4
Dean Agan <agandfr@gmail.com>
2020-11-16 11:13:27 +1100

    Add test

---
+++ test/test_sort_reverse_ascending.py
@@ -0,0 +1,16 @@
+import pytest
+
+from src.sort_reverse_ascending import reverse_ascending
+
+
+@pytest.mark.parametrize('input, expected', [
+    ([1, 2, 3, 4, 5],  [5, 4, 3, 2, 1]),
+    ([5, 7, 10, 4, 2, 7, 8, 1, 3],  [10, 7, 5, 4, 8, 7, 2, 3, 1]),
+    ([5, 4, 3, 2, 1],  [5, 4, 3, 2, 1]),
+    ([],  []),
+    ([1],  [1]),
+    ([1, 1],  [1, 1]),
+    ([1, 1, 2],  [1, 2, 1]),
+])
+def test_sort_except_zeros(input, expected):
+    assert reverse_ascending(input) == expected
