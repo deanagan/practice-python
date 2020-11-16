3e4966863c2c772b60ada2b3fd8bc4a701ddc1f7
Dean Agan <agandfr@gmail.com>
2020-11-16 14:30:58 +1100

    Add new test

--- test/test_sort_reverse_ascending.py
+++ test/test_sort_reverse_ascending.py
@@ -11,6 +11,7 @@ from src.sort_reverse_ascending import reverse_ascending
     ([1],  [1]),
     ([1, 1],  [1, 1]),
     ([1, 1, 2],  [1, 2, 1]),
+    ([1, 2, 2, 3],  [2, 1, 3, 2]),
 ])
 def test_sort_except_zeros(input, expected):
     assert reverse_ascending(input) == expected
