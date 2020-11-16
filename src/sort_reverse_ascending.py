6cb7a3d77cb62effb380b16082d51e712a1562a3
Dean Agan <agandfr@gmail.com>
2020-11-16 11:13:43 +1100

    initial brute force solution

---
+++ src/sort_reverse_ascending.py
@@ -0,0 +1,10 @@
+def reverse_ascending(items):
+	res, d = [], []
+	for i,n in enumerate(items):
+
+		if d and d[-1] > n:
+			res.extend(list(sorted(d, reverse=True)))
+			d.clear()
+		d.append(n)
+	res.extend(sorted(d, reverse=True))
+	return res
