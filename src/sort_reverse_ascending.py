6a97866cf9e3b9f852d0ef8ad50256a9839feeb0
Dean Agan <agandfr@gmail.com>
2020-11-16 12:36:56 +1100

    Fixed failing test

--- src/sort_reverse_ascending.py
+++ src/sort_reverse_ascending.py
@@ -2,7 +2,7 @@ def reverse_ascending(items):
 	res, d = [], []
 	for i,n in enumerate(items):
 
-		if d and d[-1] > n:
+		if d and d[-1] >= n:
 			res.extend(list(sorted(d, reverse=True)))
 			d.clear()
 		d.append(n)
