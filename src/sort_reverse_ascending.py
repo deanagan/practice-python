1d00c03fedd6f89701822a416c6c6d622fed1778
Dean Agan <agandfr@gmail.com>
2020-11-16 15:00:30 +1100

    remove unused

--- src/sort_reverse_ascending.py
+++ src/sort_reverse_ascending.py
@@ -2,11 +2,11 @@ from heapq import heappush, heappop
 
 def reverse_ascending(items):
     def pop_positives(subitems):
-        yield from (-1 * heappop(subitems) for _ in range(len(subitems)))
+        yield from (abs(heappop(subitems)) for _ in range(len(subitems)))
 
     result, temp = [], []
-    for i, n in enumerate(items):
-        if temp and (-1*temp[0]) >= n:
+    for n in items:
+        if temp and abs(temp[0]) >= n:
             result.extend(pop_positives(temp))
         heappush(temp, -n)
 
