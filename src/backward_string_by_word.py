890a28bbca17b358578aa5fab7e790f10299c457
Dean Agan <agandfr@gmail.com>
2020-11-20 17:51:55 +1100

    Comment out unused

--- src/backward_string_by_word.py
+++ src/backward_string_by_word.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+#from itertools import groupby
 
 def backward_string_by_word(text: str) -> str:
     #return ''.join(''.join(v)[::-1] for _,v in groupby(text, lambda w: w.isspace())) # Keeping this solution here for reference
