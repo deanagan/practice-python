aa6db53d6b9079d0249d08caa9338cb91141290a
Dean Agan <agandfr@gmail.com>
2020-11-21 20:37:53 +1100

    Fixed backtracking solution

--- src/cipher_crossword.py
+++ src/cipher_crossword.py
@@ -1,44 +1,73 @@
-from collections import Counter
-from collections import deque
-from itertools import chain, combinations, product, permutations, groupby
+from itertools import chain
 
-''' word can exist in one of the rows if a row matches the word or row is all numbers '''
-def can_word_exist_in_row(puzzle, word):
-    return any( all(a==b or isinstance(a, int) for a,b in zip(row, word)) for row in puzzle )
+def all_rows_can_match_word(puzzle, words):
+    for row in puzzle:
+        if ' ' in row:
+            continue
+        if not any(all(a==b or isinstance(a,int) for a,b in zip(row, word)) for word in words):
+            return False
+    return True
 
-''' word can exist in one of the columns if a column matches the word or column is all numbers '''
-def can_word_exist_in_colum(puzzle, word):
-    columns = [[i[t] for i in puzzle] for t in range(len(puzzle[0]))]
-    return any( all([a == b or isinstance(a, int) for a,b in zip(column, word)]) for column in columns )
+def all_columns_can_match_word(puzzle, words):
+    columns = ([i[t] for i in puzzle] for t in range(len(puzzle[0])))
+    for column in columns:
+        if ' ' in column:
+            continue
+        if not any(all(a==b or isinstance(a,int) for a,b in zip(column, word)) for word in words):
+            return False
+    return True
 
 def do_all_words_exist_in_puzzle(puzzle, words):
 
     if any(isinstance(e, int) for e in chain.from_iterable(puzzle)):
         return False
 
-    checklist = deque(words)
     # Read words by row
-    found_words = [''.join(row) for row in puzzle]
+    found_words = [''.join(ch for ch in row if isinstance(ch, str)) for row in puzzle]
     # Read words by column
-    found_words.extend([''.join([i[t] for i in puzzle]) for t in range(len(puzzle[0]))])
+    found_words.extend([''.join([i[t] if isinstance(i[t], str) else '' for i in puzzle]) for t in range(len(puzzle[0]))])
+
     return all(word in found_words for word in words)
 
-def replace_all(puzzle, number, letter):
+def replace_all(puzzle,/, dest, replacement):
     for row_number, row_elements in enumerate(puzzle):
         for column_number, value in enumerate(row_elements):
-            if number == value:
-                puzzle[row_number][column_number] = letter
+            if dest == value:
+                puzzle[row_number][column_number] = replacement
+
+
+def potential_solution(puzzle, words):
+    return all_columns_can_match_word(puzzle, words) and all_rows_can_match_word(puzzle, words)
+
+
+def solve(puzzle, words):
+
+    if do_all_words_exist_in_puzzle(puzzle, words):
+        return True
+
+    if not potential_solution(puzzle, words):
+        return False
+
+    letters = (ch for ch in set(''.join(words)) if ch not in chain.from_iterable(puzzle))
+    numbers = (num for num in chain.from_iterable(puzzle) if isinstance(num, int))
+
+    for number in numbers:
+        for letter in letters:
+
+            replace_all(puzzle, number, letter)
+            if potential_solution(puzzle, words) and solve(puzzle, words):
+                return True
+            else:
+                replace_all(puzzle, letter, number) # backtrack
+    return False
+
 
 def cipher(puzzle, words):
-
-    return [['h', 'e', 'l', 'l', 'o'],
-            ['a', ' ', 'e', ' ', 'z'],
-            ['b', 'i', 'm', 'b', 'o'],
-            ['i', ' ', 'm', ' ', 'n'],
-            ['t', 'r', 'a', 'c', 'e']]
-
+    replace_all(puzzle, dest = 0, replacement=' ')
+    return puzzle if solve(puzzle, words) else []
 
 if __name__ == '__main__':
+
     puzzle = [
                 [21, 6, 25, 25, 17],
                 [14, 0, 6, 0, 2],
@@ -49,9 +78,12 @@ if __name__ == '__main__':
     words = ['hello', 'habit', 'lemma', 'ozone', 'bimbo', 'trace']
 
     result = cipher(puzzle, words)
+    print(result)
 
     expected = [['h', 'e', 'l', 'l', 'o'],
                 ['a', ' ', 'e', ' ', 'z'],
                 ['b', 'i', 'm', 'b', 'o'],
                 ['i', ' ', 'm', ' ', 'n'],
                 ['t', 'r', 'a', 'c', 'e']]
+
+    #assert(result == expected)
