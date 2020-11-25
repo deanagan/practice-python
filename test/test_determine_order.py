4b71ced6efa6252e76de5f9fd0b5e56996903ded
Dean Agan <agandfr@gmail.com>
2020-11-25 20:42:56 +1100

    Add more tests

--- test/test_determine_order.py
+++ test/test_determine_order.py
@@ -4,7 +4,17 @@ from src.determine_order import determine_order
 
 
 @pytest.mark.parametrize(('input, expected'), [
+    (["jhgedba","jihcba","jigfdca"], "jihgefdcba"),
+    (["ghi","abc","def"], "abcdefghi"),
+    (["b", "d", "a"], "abd"),
     (["axton","bxton"], "abxton"),
+    (["acb", "bd", "zwa"], "zwacbd"),
+    (["klm", "kadl", "lsm"], "kadlsm"),
+    (["a", "b", "c"], "abc"),
+    (["aazzss"], "azs"),
+    (["dfg", "frt", "tyg"], "dfrtyg"),
+    (["xxxyyz", "yyww", "wwtt", "ttzz"], "xywtz"),
+    (["jhgfdba","jihcba","jigedca"], "jihgefdcba"),
 ])
 def test_how_deep(input, expected: str):
     assert determine_order(input) == expected
