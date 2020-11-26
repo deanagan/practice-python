5afe1a9806b5116bc560e526ff50ef4bba337615
Dean Agan <agandfr@gmail.com>
2020-11-26 20:41:45 +1100

    parametrize test

--- test/test_three_words.py
+++ test/test_three_words.py
@@ -2,10 +2,13 @@ import pytest
 
 from src.three_words import has_3digitless_consecutive
 
-def test_three_words():
 
-    assert has_3digitless_consecutive("Hello World hello") == True, "Hello"
-    assert has_3digitless_consecutive("He is 123 man") == False, "123 man"
-    assert has_3digitless_consecutive("1 2 3 4") == False, "Digits"
-    assert has_3digitless_consecutive("bla bla bla bla") == True, "Bla Bla"
-    assert has_3digitless_consecutive("Hi") == False, "Hi"
+@pytest.mark.parametrize(('input, expected, note'), [
+    ("Hello World hello", True, "Hello"),
+    ("He is 123 man", False, "123 man"),
+    ("1 2 3 4", False, "Digits"),
+    ("bla bla bla bla", True, "Bla Bla"),
+    ("Hi", False, "Hi"),
+])
+def test_three_words(input, expected, note):
+    assert has_3digitless_consecutive(input) == expected, note
