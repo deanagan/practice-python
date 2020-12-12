7f48b6da5ec79587d118e8585f23bf8de8bc6554
Dean Agan <agandfr@gmail.com>
2020-12-12 19:49:23 +1100

    Cleanup + refactoring

--- src/sort_by_extension.py
+++ src/sort_by_extension.py
@@ -17,12 +17,7 @@ Output: A list of filenames.
 '''
 from typing import List
 def sort_by_ext(filenames: List[str]) -> List[str]:
-    by_no_extension = lambda w: 0 if w.endswith('.') else w[1:-1].count('.')
-    by_ext_name = lambda word: word[-word[::-1].index('.'):]
-    by_name = lambda word: word
+    by_no_extension = lambda word: 0 if word.endswith('.') else word[1:-1].count('.')
+    by_ext_name = lambda word: word[word.rindex('.') + 1:] or word
 
-    return sorted(filenames, key = lambda word: (by_no_extension(word), by_ext_name(word), by_name(word)))
-
-
-if __name__ == "__main__":
-     assert sort_by_ext(['1.cad', '1.bat', '1.aa', '.aa.doc']) ==  ['1.aa', '1.bat', '1.cad', '.aa.doc']
+    return sorted(filenames, key = lambda word: (by_no_extension(word), by_ext_name(word), word))
