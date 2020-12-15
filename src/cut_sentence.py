484f2a16d0b39c42ed3b1a1235fa748ecf081490
Dean Agan <agandfr@gmail.com>
2020-12-15 20:14:17 +1100

    Clean up. Shorter solution

--- src/cut_sentence.py
+++ src/cut_sentence.py
@@ -1,16 +1,11 @@
-from itertools import takewhile as tw
+
 
 
 def cut_sentence(line: str, length: int) -> str:
-    cut = ' '.join(tw(lambda word: word in line.split(), line[:length].split()))
-    return cut + '...' if len(line) > length else cut
+    '''
+    Cut a given sentence, so it becomes shorter than or equal to a given length.
+    '''
+    if len(line) <= length:
+        return line
 
-
-
-if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert cut_sentence("Hi my name is Alex", 4) == "Hi...", "First"
-    assert cut_sentence("Hi my name is Alex", 8) == "Hi my...", "Second"
-    assert cut_sentence("Hi my name is Alex", 18) == "Hi my name is Alex", "Third"
-    assert cut_sentence("Hi my name is Alex", 20) == "Hi my name is Alex", "Fourth"
-    assert cut_sentence("Hi my name is Alex", 11) == "Hi my name...", "Fifth"
+    return line[:max(0,line[:length+1].rfind(' '))] + '...'
