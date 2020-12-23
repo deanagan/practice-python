c11f2bea8c78a727ebd4e31244f6c6f3c9919d12
Dean Agan <agandfr@gmail.com>
2020-12-23 21:21:41 +1100

    Add test

--- test/test_zig_zag.py
+++ test/test_zig_zag.py
@@ -5,7 +5,8 @@ from src.zig_zag import ZigZag
 
 @pytest.mark.parametrize(("input, rows, expected"), [
     ('PAYPALISHIRING', 3, 'PAHNAPLSIIGYIR'),
-    ('PAYPALISHIRING', 4, 'PINALSIGYAHRPI')
+    ('PAYPALISHIRING', 4, 'PINALSIGYAHRPI'),
+    ('A', 1, 'A')
 ])
 def test_zig_zag(input, rows, expected):
     sut = ZigZag()
