acba1ed1809620bd226bc90f1f40cf842b698ed3
Dean Agan <agandfr@gmail.com>
2020-12-25 09:04:08 +1100

    initial slow solution

--- src/container_with_most_water.py
+++ src/container_with_most_water.py
@@ -1,5 +1,6 @@
 from typing import List
+from itertools import combinations
 
 class Solution:
     def max_area(self, height: List[int]) -> int:
-        return 0
+        return max(abs(x-y)* min(height[x], height[y]) for x,y in combinations(range(len(height)), r=2))
