02cc4f067e68b3c5d1f07ab6877737211eab9ba8
Dean Agan <agandfr@gmail.com>
2020-12-25 13:57:48 +1100

    Change solution to be more efficient

--- src/container_with_most_water.py
+++ src/container_with_most_water.py
@@ -1,6 +1,21 @@
-from typing import List
-from itertools import combinations
+from typing import List, Generator
+
 
 class Solution:
+
+    @classmethod
+    def paired(cls, height) -> Generator:
+        left = 0
+        right = len(height) - 1
+        while left != right:
+            left_height, right_height = height[left], height[right]
+            smaller = min(left_height, right_height)
+            yield abs(left - right) * smaller
+            if left_height < right_height:
+                left += 1
+            else:
+                right -= 1
+
+
     def max_area(self, height: List[int]) -> int:
-        return max(abs(x-y)* min(height[x], height[y]) for x,y in combinations(range(len(height)), r=2))
+        return max(self.paired(height))
