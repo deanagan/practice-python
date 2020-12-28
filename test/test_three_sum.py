9ee82a99ab02f6e1d0c96be9cc506db26194173f
Dean Agan <agandfr@gmail.com>
2020-12-28 12:11:41 +1100

    Add tests

---
+++ test/test_three_sum.py
@@ -0,0 +1,12 @@
+import pytest
+from src.three_sum import Solution
+from typing import List
+
+@pytest.mark.parametrize('input, expected', [
+    ([-1,0,1,2,-1,-4], [[-1,-1,2],[-1,0,1]]),
+    ([0], []),
+    ([], []),
+])
+def test_three_sum(input: List[int], expected: List[List[int]]):
+    sut = Solution()
+    assert sorted(sut.three_sum(input)) == sorted(expected)
