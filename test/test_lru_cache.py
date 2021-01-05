ea19f6c228fa6e8dc81e97ee550f7714605da654
Dean Agan <agandfr@gmail.com>
2021-01-05 16:05:52 +1100

    Initial test

---
+++ test/test_lru_cache.py
@@ -0,0 +1,17 @@
+import pytest
+
+from src.lru_cache import LRUCache
+from collections.abc import Iterable
+from collections import deque
+
+@pytest.mark.parametrize('capacity, input, expected', [
+    (2, [(1,1), (2,2), 1, (3,3), 2, (4,4), 1, 3, 4], [1,-1,-1, 3, 4]),
+])
+def test_lru_cache(capacity, input, expected):
+    sut = LRUCache(capacity)
+    get_expectations = deque(expected)
+    for args in input:
+        if isinstance(args, Iterable):
+            sut.put(*args)
+        else:
+            assert sut.get(args) == get_expectations.popleft()
