624e6067214341017b7d4bae3c41791c073696dc
Dean Agan <agandfr@gmail.com>
2021-01-05 16:29:22 +1100

    Add tests

--- test/test_lru_cache.py
+++ test/test_lru_cache.py
@@ -6,6 +6,8 @@ from collections import deque
 
 @pytest.mark.parametrize('capacity, input, expected', [
     (2, [(1,1), (2,2), 1, (3,3), 2, (4,4), 1, 3, 4], [1,-1,-1, 3, 4]),
+    (2,[(2,1),(2,2), 2, (1,1), (4,1), 2], [2, -1]),
+    (2, [(2,1), (1,1), (2,3), (4,1), 1, 2], [-1, 3])
 ])
 def test_lru_cache(capacity, input, expected):
     sut = LRUCache(capacity)
@@ -15,3 +17,5 @@ def test_lru_cache(capacity, input, expected):
             sut.put(*args)
         else:
             assert sut.get(args) == get_expectations.popleft()
+
+    assert len(get_expectations) == 0
