217592f4de3eeeb503495bfe4addfe7031a5da68
Dean Agan <agandfr@gmail.com>
2021-01-06 21:51:24 +1100

    Fixed annotation to use optional return either list of int or none.

--- src/find_second_index.py
+++ src/find_second_index.py
@@ -1,5 +1,5 @@
+from typing import Optional, List
 
 
-
-def second_index(text: str, symbol: str) -> [int, None]:
+def second_index(text: str, symbol: str) -> Optional[List[int]]:
     return [i for i,ch in enumerate(text) if ch ==symbol][1] if text.count(symbol) > 1 else None
