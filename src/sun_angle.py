5e232eb5aacca62a82de5c9ef7755af1045732ba
Dean Agan <agandfr@gmail.com>
2021-01-07 19:02:11 +1100

    Use union to indicate return is either float or string

--- src/sun_angle.py
+++ src/sun_angle.py
@@ -1,7 +1,7 @@
 from datetime import datetime as dt
+from typing import Union
 
-
-def sun_angle(time: str) -> [float, str]:
+def sun_angle(time: str) -> Union[float, str]:
     ref_time = dt.strptime('06:00', '%H:%M')
     fmt_time = dt.strptime(time, '%H:%M')
     angle = (fmt_time - ref_time).seconds / 60 * 0.25
