3e534d50813ec5919f7535328b9601483544e529
Dean Agan <agandfr@gmail.com>
2021-01-10 20:05:43 +1100

    Typing annotations

--- src/moore_neighbourhood.py
+++ src/moore_neighbourhood.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from itertools import product
 
+Grid = tuple[tuple[int]]
 
-def count(grid, row, col):
+def count(grid: Grid, row: int, col: int) -> int:
     nbrs = [(rowoffset,coloffset) for rowoffset, coloffset in product(range(-1,2), repeat=2) if rowoffset or coloffset]
     return sum([grid[nrow+row][ncol+col] for nrow,ncol in nbrs if 0 <= nrow+row < len(grid) and 0 <= ncol+col < len(grid[0])])
