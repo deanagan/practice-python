3ebe151b51f23d6db11df0c7d75e5b3952285b7a
Dean Agan <agandfr@gmail.com>
2021-01-10 20:16:44 +1100

    Fixed typing

--- src/moore_neighbourhood.py
+++ src/moore_neighbourhood.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from itertools import product
+from typing import Iterable
 
-Grid = tuple[tuple[int]]
+Grid = Iterable[Iterable[int]]
 
 def count(grid: Grid, row: int, col: int) -> int:
     nbrs = [(rowoffset,coloffset) for rowoffset, coloffset in product(range(-1,2), repeat=2) if rowoffset or coloffset]
